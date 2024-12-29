54edb7c422ffebc13cb2fb78d90753bc3e9d5842
Vaishnavi124 <84326255+Vaishnavi124@users.noreply.github.com>
2024-12-29 16:48:56 +0530

    Updated ALLOWED_HOSTS in settings.py

--- djangopro1/settings.py
+++ djangopro1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@m#%0pqyr7hz(*k5kb7bs9#kcr3dlbk)tah2fh81rh)_%lbs3p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['TechSchool.vercel.app']
 
 
 # Application definition
